be91852654d3561e22ee5f3bf40a13619fb8f1ef
gamak grover <gamakgroverofficial@gmail.com>
2024-11-15 01:43:46 +0530

    feat: header

--- blog-app/src/components/entities/Header/index.tsx
+++ blog-app/src/components/entities/Header/index.tsx
@@ -1,12 +1,24 @@
-import { ImageType } from "@models";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import Image from "next/image";
 import React from "react";
 
 import { styles } from "./styles";
 
-export type HeaderProps = {
-  logo: ImageType;
-};
 export function Header() {
-  return <Box sx={styles.wrapper}>LOGO</Box>;
+  return (
+    <Box sx={styles.wrapper}>
+      <Box>
+        <Image width={50} height={50} alt="logo" src="/icons/logo.png"></Image>
+      </Box>
+      <Box sx={styles.menu}>
+        <p>Home</p>
+        <p>Blogs</p>
+        <p>Contact</p>
+      </Box>
+      <Box sx={styles.buttons}>
+        <Button variant="contained">Log in</Button>
+        <Button>Sign up</Button>
+      </Box>
+    </Box>
+  );
 }
--- blog-app/src/components/entities/Header/styles.ts
+++ blog-app/src/components/entities/Header/styles.ts
@@ -1,5 +1,17 @@
 export const styles = {
   wrapper: {
     width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "5px 10px",
+  },
+  menu: {
+    display: "flex",
+    gap: "50px",
+  },
+  buttons: {
+    display: "flex",
+    gap: "50px",
+    justifyContent: "space-around",
   },
 };
